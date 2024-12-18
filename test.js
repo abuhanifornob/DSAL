f493a0108fec61a669c6a1567395d407b95a18f5
Abu Hanif <hanifcse90@gmail.com>
2024-12-18 11:44:28 +0600

    fist commit

---
+++ test.js
@@ -0,0 +1,22 @@
+function factorialNumber(n){
+    if(n===1 || n===2){
+        return 1;
+    }
+    else{
+        return n*factorialNumber(n-1);
+    }
+    
+}
+
+const factorial=factorialNumber(5);
+console.log(factorial);
+
+function fibonacci(n){
+    let fib=[0,1];
+    for(let i=2;i<=n;i++){
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+    return fib.slice(0,n+1);
+}
+
+console.log(fibonacci(5));
